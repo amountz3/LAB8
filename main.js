1749339790f902e2a64958c3450bef8019921c02
alyssa mountz <a.m.mountz3@gmail.com>
2017-07-18 19:37:33 -0400

    changed js file

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var list = [];
 $( "form" ).submit(function( event ) {
 event.preventDefault();
-    var fn = document.getElementById("FN").val;
-    var ln =  document.getElementById("LN").val;
-    var sn =  document.getElementById("SN").val;
+    var fn = $('#FN').val();
+    var ln =  $('#LN').val();
+    var sn =  $('#SN').val();
 list.push({fn: fn, ln: ln, sn : sn});
 console.log(list);
 });
