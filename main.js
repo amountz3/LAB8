250e0e31e395beb42b3c8a5c0333b0616dfce1f6
Andrew Hayes <andy.hayes13@gmail.com>
2017-07-18 19:09:23 -0400

    uploading all files

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var seatNumber = document.getElementsByName("seatnumber");
+var list = [];
+$( "form" ).submit(function( event ) {
+event.preventDefault();
+    var fn = document.getElementById("FN").val;
+    var ln =  document.getElementById("LN").val;
+    var sn =  document.getElementById("SN").val;
+list.push({fn: fn, ln: ln, sn : sn});
+console.log(list);
+});
