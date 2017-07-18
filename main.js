48eca1c417ddbfd1e75faea1ade203df42b30ff7
alyssa mountz <a.m.mountz3@gmail.com>
2017-07-18 19:05:51 -0400

    modified JS so the object accepted input info into an array

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var seatNumber = document.getElementsByName("seatnumber");
+
+var list = [];
+$( "form" ).submit(function( event ) {
+event.preventDefault();
+    var fn = document.getElementById("FN").val;
+    var ln =  document.getElementById("LN").val;
+    var sn =  document.getElementById("SN").val;
+list.push({fn: fn, ln: ln, sn : sn});
+console.log(list);
+});
